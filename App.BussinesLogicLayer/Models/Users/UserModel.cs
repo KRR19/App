98cf88c9270e0ce8fdb637f37d87f624085d4530
Роман Казаров <kazarovroman@gmail.com>
2019-10-15 13:35:58 +0300

    account

--- App.BussinesLogicLayer/Models/Users/UserModel.cs
+++ App.BussinesLogicLayer/Models/Users/UserModel.cs
@@ -5,13 +5,14 @@ using Microsoft.AspNetCore.Identity;
 
 namespace App.BussinesLogicLayer.Models.Users
 {
-    public class UserModel
+    public class UserModel : IdentityUser
     {
-        public string UserName { get; set; }
+       // public string UserName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string PasswordHash { get; set; }
-        public string Email { get; set; }
+        //public string PasswordHash { get; set; }
+       // public string Email { get; set; }
         public string Role { get; set; }
+        public string Password { get; set; }
     }
 }
