8bc57ceb68a41826259fdbf16db0a664d3ad7929
Роман Казаров <kazarovroman@gmail.com>
2019-10-16 17:49:38 +0300

    PaswordReset

---
+++ App.BussinesLogicLayer/Models/Users/ResetPasswordModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace App.BussinesLogicLayer.Models.Users
+{
+    public class ResetPasswordModel
+    {
+        public string Email { get; set; }
+                
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        public string Code { get; set; }
+    }
+}
