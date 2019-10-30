9c7040324941f5dd26c9865398f869916de9a377
Роман Казаров <kazarovroman@gmail.com>
2019-10-30 11:47:37 +0200

    BaseResponseModel init

---
+++ Client/src/app/printing-edition/printing-edition.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-printing-edition',
+  templateUrl: './printing-edition.component.html',
+  styleUrls: ['./printing-edition.component.scss']
+})
+export class PrintingEditionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
