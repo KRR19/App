197c6b943eb23506798385f33de62d5039c21690
Роман Казаров <kazarovroman@gmail.com>
2019-11-15 15:58:44 +0200

    Client

--- App.BussinesLogicLayer/models/Authors/AuthorModel.cs
+++ App.BussinesLogicLayer/models/Authors/AuthorModel.cs
@@ -9,7 +9,5 @@ namespace App.BussinesLogicLayer.models.Authors
         public string Name { get; set; }
         public DateTime DateBirth { get; set; }
         public DateTime DateDeath { get; set; }
-        public List<Guid> AuthorId { get; set; }
-        public List<string> AuthorName { get; set; }
     }
 }
